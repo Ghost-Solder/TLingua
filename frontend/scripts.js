1a5a6bf83ab8497116e4888fa256721f7dbd6c6e
dosipov <ghost-solder@yandex.ru>
2024-07-18 19:46:18 +0300

    change design

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,21 +1,13 @@
-// Проверка, загружен ли скрипт Telegram Web App
 if (Telegram.WebApp) {
     Telegram.WebApp.ready();
-
-    // Получаем данные пользователя
-    const user = Telegram.WebModals.initDataUnsafe.user;
-
-    // Заполняем информацию о пользователе
+    const user = Telegram.WebApp.initDataUnsafe.user;
     const userNameElement = document.getElementById('userName');
     const userAvatarElement = document.getElementById('userAvatar');
-
     if (user) {
         userNameElement.textContent = `${user.first_name} ${user.last_name}`;
-        userAvatarElement.src = user.photo_url || 'images/default-avatar.png'; // Укажите путь к изображению по умолчанию.
-        // Дополнительная логика после получения данных пользователя
+        userAvatarElement.src = user.photo_url || 'images/default-avatar.png';
     } else {
         userNameElement.textContent = 'User information not available.';
-        // Можно отобразить сообщение об ошибке или скрыть элементы
     }
 } else {
     console.error('Telegram Web App SDK not loaded!');
